22730e98761e8e12c5416f17dd673ae0a374e120
fs-rd-yu-fukuzawa <yu.fukuzawa@f-scratch.com>
2019-03-01 19:29:27 +0900

    Vue plugin setting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+// import Vue from 'vue'
+import * as components from './components'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+var ZeldaVuestory = {
+  install: function (Vue, opts = {}) {
+    // コンポーネントの登録
+    (function registerComponents (components) {
+      if (components) {
+        for (const key in components) {
+          const component = components[key]
+          Vue.component(key, component)
+        }
+      }
+    })(components)
+  },
+  version: '1.0'
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(ZeldaVuestory)
+}
+
+export default ZeldaVuestory
